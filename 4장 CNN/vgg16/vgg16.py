62774c3b59ef564b4e1aecf59121f70b9297a97c
eddie94 <myeddie@naver.com>
2020-07-31 15:47:21 +0900

    structure modified

--- 4장 CNN/vgg16/vgg16.py
+++ 4장 CNN/vgg16/vgg16.py
@@ -5,27 +5,12 @@ import numpy as np
 import PIL.Image as pilimg
 import PIL
 
-class VGG16():
-    def __init__(self, input_image_vector: np.ndarray, epochs: int=20, batch_size: int=100):
-        '''
-        input_image: PIL Image
-        '''
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.input_image_vector = input_image_vector
-        self.total_batch = int(len(input_image_vector)/batch_size)
+graph = tf.get_default_graph()
 
-    def batch_generator():
-        for i in range(0, self.total_batch, self.batch_size):
-            image_batch = self.input_image_vector[i:i+self.batch_size]
-            yield image_batch
-
-    @classmethod
-    # convolutions layer
-    def Convnet(input_tensor, input_channels, output_channels,
-                name="Conv", stddev=0.1, padding="SAME", activation="relu",
-                kernel_shape=(3,3), pool_shape=(2,2),
-                conv_stride=[1,1,1,1], pool_stride=[1,2,2,1]):
+# convolution layer
+def Convnet(input_tensor, input_channels, output_channels, name,
+                stddev=0.1, padding="SAME",
+                kernel_shape=(3,3), conv_stride=[1,1,1,1]):
 
         # set kernel
         kernel = tf.Variable(tf.random_normal(
@@ -44,16 +29,51 @@ class VGG16():
         )
         return Conv1
 
-    @classmethod
-    # fully connected layer
-    def FCnet(input_tensor, output_channels):
-        pass
+# fully connected layer
+def FCnet(input_tensor, output_channels, name):
+    '''
+    input tensor shape : (1, ?)
+    '''
+    W = tf.get_variable(name+"/W", shape=(input_tensor.shape.as_list()[1], output_channels),
+                        dtype=tf.float32)
+    b = tf.get_variable(name+"/b", shape=(output_channels),
+                        dtype=tf.float32)
+
+    output = tf.matmul(input_tensor, W) + b
+    return output
+
+class VGG16():
+    def __init__(self, input_image_vector: list, class_labels: list, epochs: int=20, batch_size: int=100):
+        '''
+        input_image: PIL Image
+        '''
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.input_image_vector = input_image_vector
+        self.class_labels = class_labels
+        self.total_batch = int(len(input_image_vector)/batch_size)
+
+    # returns a list of images
+    def batch_generator(self):
+        for i in range(0, self.total_batch, self.batch_size):
+            image_batch = self.input_image_vector[i:i+self.batch_size]
+            yield image_batch
 
     @classmethod
-    def train():
+    def train(self, num_classes):
         
-        X_raw = tf.placeholder(dtype=tf.float32, shape=(224,224,3))
-        X = tf.reshape(X_raw, (-1, 224, 224, 3))
+        X_raw = tf.placeholder(dtype=tf.float32, shape=(224,224,3), name="X_raw")
+        X = tf.reshape(X_raw, (-1, 224, 224, 3), name="X")
+        Y = tf.placeholder(dtype=tf.float32, shape=(None, num_classes), name="Y")
+
+        conv1 = Convnet(X, input_channels=3, output_channels=64, name="conv1_1")
+
+        with graph.as_default():
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+
+                for batch in self.batch_generator():
+                    pass
 
 def gen(numList):
     for i in range(0, len(numList), 10):
